c8dc6a45742e95f00fd0ac04cd8e8d30dc7cfd61
Frankstein97 <francoivannicoletti@gmail.com>
2022-08-18 21:54:25 -0300

    Primer Desafio

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Franktein Cafe App">
+      <header className="App-header">
+      <div style={{display: "flex"}}>
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        </div>
+<div style={{display: "flex"}}>
+     <p className="App-logo"> Sin television y sin cerveza </p> 
+     <p className="App-logo"> homero pierde la cabevza </p>
+     <p className="App-logo"> Sin television y sin cerveza </p> 
+     <p className="App-logo"> homero pierde la cabevza </p>
+     <p className="App-logo"> Sin television y sin cerveza </p> 
+     <p className="App-logo"> homero pierde la cabevza </p>
+     </div>
+        <div style={{display: "flex"}}>
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
