a28478f626c0aefc6ce45130904e8f76e930c7bc
Frankstein97 <francoivannicoletti@gmail.com>
2022-08-29 21:58:23 -0300

    segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="Franktein Cafe App">
+
+      <NavBar/>
+      
       <header className="App-header">
       <div style={{display: "flex"}}>
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+    
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid ">
+    <a className="navbar-brand me-5 " href="/#">
+      <img src="https://acegif.com/wp-content/uploads/gifs/coffee-88.gif" alt="" width="90" className="d-inline-block align-text-top"/></a>
+    <a className="navbar-brand me-5 " href="#/" style={{fontSize: "2rem"}}>Cafe App</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item me-5">
+          <a className="nav-link" href="#/">NOSOTROS</a>
+        </li>
+        <li className="nav-item me-5">
+          <a className="nav-link" href="#/">PRODUCTOS</a>
+        </li>
+        <li className="nav-item me-5">
+          <a className="nav-link" href="#/">FILTRADOS</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Productos y filtrados..." aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    
+    </>
+  )
+}
+
+export default NavBar
